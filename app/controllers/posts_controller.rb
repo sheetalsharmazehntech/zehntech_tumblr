4d5a59511cb07ad39f68138594da0e9ef70d5569
sheetalsharmazehntech <sheetal.sharma@zehntech.com>
2022-10-07 11:37:34 +0530

    fix some bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,22 @@ end
 def new
 end   
 
+ def create
+    @post = Post.new(post_params)
+        respond_to do |f|
+        if @post.save
+            redirect_to @post, notice: "Your post has been saved"
+        else
+            render :create
+        end
+    end
+ end
+
+  private
+
+def post_params
+    params.require(:post).permit(:title, :body)
+end
+
 
 end
